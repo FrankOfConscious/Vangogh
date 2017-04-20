1c878efb544a0fdaa93b09b985e7fe4365acf46c
FrankOfConscious <yliao3@student.unimelb.edu.au>
2017-04-20 15:00:02 +1000

    Update KeyTuple.java

--- KeyTuple.java
+++ KeyTuple.java
@@ -1,5 +1,9 @@
 package Server;
 
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+
 public class KeyTuple<A,B,C,D> {
 	/** <p>Title: TwoTuple</p>
 	 * <p>Description: 三个元素的元组，用于在一个方法里返回两种类型的值</p>
@@ -18,7 +22,7 @@ public class KeyTuple<A,B,C,D> {
 	        this.obj = obj;
 	    
 	}
-	    boolean ifOverwrites(KeyTuple old){//比较三个primary key，如果都相同，则返回true否则返回false。都相同需要overwrites操作
+	    boolean ifOverwrites(KeyTuple old){
 	    	if(this.key1.equals(old.getOwner())
 	    			&& this.key2.equals(old.getChannel())
 	    			&& this.key3.equals(old.getUri()))
@@ -26,10 +30,24 @@ public class KeyTuple<A,B,C,D> {
 	    	else return false;
 	    	
 	    }
-	    //如果要比较的话，需要给command和resource转换成KeyTuple，但是command里面不含obj啊
-	    boolean ifduplicated(KeyTuple old){//比较 channel 和uri 是否都相同，如果都相同则返回true，说明违反了publish的倒数第三条规则
+	    boolean ifOverwrites(JSONObject command){
+	    	if(this.key1.equals(((HashMap) command.get("resource")).get("owner"))
+	    			&& this.key2.equals(((HashMap) command.get("resource")).get("channel"))
+	    			&& this.key3.equals(((HashMap) command.get("resource")).get("uri")))
+	    		return true;
+	    	else return false;
+	    	
+	    }
+	    
+	    boolean ifduplicated(KeyTuple old){
 	    	if(this.key2.equals(old.getChannel())
 	    			&& this.key3.equals(old.getUri()))
+	    		return true;
+	    	else return false;
+	    }
+	    boolean ifduplicated(JSONObject command){
+	    	if(this.key2.equals(((HashMap) command.get("resource")).get("channel"))
+	    			&& this.key3.equals(((HashMap) command.get("resource")).get("uri")))
 	    		return true;
 	    	else return false;
 	    }
