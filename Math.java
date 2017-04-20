8dfd7d73c287cac81f3624aed854c4fb45711a93
LiamZ95 <liamz95@outlook.com>
2017-04-20 21:08:10 +1000

    Update Math.java

--- Math.java
+++ Math.java
@@ -93,6 +93,61 @@ public class Math {
 		return result;
 	}
 	
+	private static JSONObject shareJSON(JSONObject command) {
+		JSONObject result = new JSONObject();
+		if(!command.containsKey("resource")||
+				!command.containsKey("secret") ||
+				!((HashMap) command.get("resource")).containsKey("owner")||
+				!((HashMap) command.get("resource")).containsKey("channel")||
+				!((HashMap) command.get("resource")).containsKey("uri")){
+			result.put("response", "error");
+			result.put("errorMessage", "missing resource and\\/or secret");
+			return result;
+		} else if(((HashMap) command.get("resource")).get("owner").equals("*")){
+			result.put("response", "error");
+			result.put("errorMessage", "invalid resource");
+			return result;
+		} else {
+			//this checks whether 
+			boolean eligible = false;
+			for (int i = 0; i < Server.secretList.size(); i++) {
+				if (Server.secretList.get(i) == command.get("secret")) {
+					eligible = true;
+				}
+			}
+			if (!eligible) {
+				result.put("response", "error");
+				result.put("errorMessage", "incorrect secret");
+				return result;
+			}
+		}
+		//this if clause check if the file scheme is "file"
+		if(!((String)((HashMap) command.get("resource")).get("uri")).substring(0, 4).equals("file") ||
+				((String)((HashMap) command.get("resource")).get("uri")).charAt(0) == '/') {
+			result.put("response", "error");
+			result.put("errorMessage", "cannot publish resource");
+			return result;
+		} else {
+			for (int i = 0; i < Server.resourceList.size(); i++) {
+				//this if clause check if there is resource with same channel and uri but different owner
+				if (Server.resourceList.get(i).ifduplicated(command)) {
+					result.put("response", "error");
+					result.put("errorMessage", "cannot publish resource");
+					return result;
+				}
+				//this checks if there is resource with same channel, uri and owner, replace the obj
+				if (Server.resourceList.get(i).ifOverwrites(command)) {
+					Server.resourceList.get(i).overwrites(command);
+					result.put("response", "success");
+					return result;
+				}	
+			}
+			Server.resourceList.add(new KeyTuple(new Resource(command)));
+			result.put("response", "success");
+		}
+		return result;
+	}
+	
 	private static JSONObject removeJSON(JSONObject command){
 		JSONObject result= new JSONObject();
 		if(!command.containsKey("resource")||
